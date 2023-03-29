1945aff3642b26cc58f1f87c85331cbf5c828f2b
Tim Deslo <timdeslo@gmail.com>
2023-03-28 22:42:15 -0400

    create-backend

--- client/src/Signin/LoginInButton.js
+++ client/src/Signin/LoginInButton.js
@@ -1,9 +1,10 @@
 import React from "react";
-import {useAuth0} from "@auth0/auth0-react";
+import { useAuth0 } from "@auth0/auth0-react";
 
-const LoginInButton = () => {
-  const {loginWithRedirect} = useAuth0();
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+const LoginButton = () => {
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
+
+  return !isAuthenticated && <button onClick={() => loginWithRedirect()}>Log In</button>;
 };
 
-export default LoginInButton;
+export default LoginButton;
--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,21 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
-import {Auth0Provider} from "@auth0/auth0-react";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const domain = process.env.REACT_APP_AUTH0_DOMAIN
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
 
-root.render(
+console.log(domain);
+ReactDOM.render(
   <Auth0Provider
     domain={domain}
     clientId={clientId}
-    redirect_uri={window.location.origin}
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
   >
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Auth0Provider>
+    <App />
+  </Auth0Provider>,
+  document.getElementById("root")
 );
